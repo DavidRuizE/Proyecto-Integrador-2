46474d5425afb495c29905427b682ba4b3b762a9
David <david02ruiz@gmail.com>
2024-04-03 12:44:35 -0500

    MVP

--- BotellasDeAmor/APP/admin.py
+++ BotellasDeAmor/APP/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class FotoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image', 'photoType', 'date', 'place']  # Include 'date' here
+
+admin.site.register(Foto, FotoAdmin)
+admin.site.register(User)
